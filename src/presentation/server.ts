e143b22b7541289fcc7b951b1a7d7d89c370231f
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-07-27 22:28:29 -0400

    Add implementation of repository pattern and configuration for environment variables

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource(),
+);
 
 export class Server {
 
@@ -8,10 +14,11 @@ export class Server {
     console.log('Server started...');
 
     CronService.createJob(
-      "*/5 * * * * *",
+      '*/5 * * * * *',
       () => {
-        const url = 'https://google.com';
+        const url = 'http://google.com';
         new CheckService(
+          fileSystemLogRepository,
           () => console.log(`${ url } is ok`),
           (error) => console.log(error),
         ).execute(url);
