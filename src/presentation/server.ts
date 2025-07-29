d5718eb7fab5996c3d38abf90dc07d35a0878acd
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-07-29 15:21:54 -0400

    Add nodemailer and basic example

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDataSource(),
@@ -14,6 +15,16 @@ export class Server {
     console.log('Server started...');
 
     // Mandar email
+    const emailService = new EmailService();
+    emailService.sendEmail({
+      to: 'jorgechavezrnd@gmail.com',
+      subject: 'Logs del sistema',
+      htmlBody: `
+        <h3> Logs del sistema - NOC</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae dolor accumsan, condimentum nisl ac, scelerisque ex. Aenean fermentum purus est, ut iaculis lorem sagittis porttitor. Vestibulum facilisis augue eu quam fringilla accumsan. Aliquam condimentum eu risus id porta. Vivamus bibendum justo lacinia rhoncus suscipit. Aenean dignissim nibh sit amet consectetur aliquet. Curabitur sed efficitur dolor, tristique sodales leo.</p>
+        <p>Ver logs adjuntos</p>
+      `
+    });
 
     // CronService.createJob(
     //   '*/5 * * * * *',
