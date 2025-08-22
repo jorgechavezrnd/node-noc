f0dbc1c116f428d5b5167d0d80f0b4073065b7d9
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-08-22 14:54:46 -0400

    Add tests for log repository

---
+++ src/infrastructure/repositories/log.repository.test.ts
@@ -0,0 +1,34 @@
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+import { LogRepositoryImpl } from './log.repository';
+
+describe('LogRepositoryImpl', () => {
+
+  const mockLogDataSource = {
+    saveLog: jest.fn(),
+    getLogs: jest.fn(),
+  };
+
+  const logRepository = new LogRepositoryImpl(mockLogDataSource);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('saveLog should call the datasource with arguments', async () => {
+
+    const log = { level: LogSeverityLevel.high, message: 'hola' } as LogEntity;
+    await logRepository.saveLog(log);
+    expect(mockLogDataSource.saveLog).toHaveBeenCalledWith(log);
+
+  });
+
+  test('getLogs should call the datasource with arguments', async () => {
+
+    const lowSeverity = LogSeverityLevel.low;
+
+    await logRepository.getLogs(lowSeverity);
+    expect(mockLogDataSource.getLogs).toHaveBeenCalledWith(lowSeverity);
+
+  });
+
+});
